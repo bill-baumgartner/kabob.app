7acfc32da98e50cfec1312a021145b09be33855f
bill-baumgartner <bill.baumgartner@gmail.com>
2016-10-19 16:06:55 -0600

    changed namespace for the kabob-base distro

--- scripts/step2_ag-setup.sh
+++ scripts/step2_ag-setup.sh
@@ -17,12 +17,12 @@ docker create --name agraph-data franzinc/agraph-data
 docker build -t ccp/agraph:v6.1.1 allegrograph/build/
 
 # Populate two Docker volumes and populate with required code:
-docker run --rm -v $(pwd):/backup ccp/kabob-base:0.1 tar czvf /backup/kabob.git-backup.tar.gz /kabob.git
+docker run --rm -v $(pwd):/backup billbaumgartner/kabob-base:0.1 tar czvf /backup/kabob.git-backup.tar.gz /kabob.git
 docker create -v /kabob.git --name kabob.git ubuntu:latest
 docker run --rm --volumes-from kabob.git -v $(pwd):/backup ubuntu:latest bash -c "cd /kabob.git && tar xzvf /backup/kabob.git-backup.tar.gz --strip 1"
 rm kabob.git-backup.tar.gz
 
-docker run --rm -v $(pwd):/backup ccp/kabob-base:0.1 tar czvf /backup/m2-backup.tar.gz /root/.m2
+docker run --rm -v $(pwd):/backup billbaumgartner/kabob-base:0.1 tar czvf /backup/m2-backup.tar.gz /root/.m2
 docker create -v /root/.m2 --name m2 ubuntu:latest
 docker run --rm --volumes-from m2 -v $(pwd):/backup ubuntu bash -c "cd /root/.m2 && tar xzvf /backup/m2-backup.tar.gz --strip 2"
 rm m2-backup.tar.gz
