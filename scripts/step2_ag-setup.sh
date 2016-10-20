fb973eb30eb9e6249222aae7afcb01135527a571
bill-baumgartner <bill.baumgartner@gmail.com>
2016-10-19 19:49:33 -0600

    max port in docker run is now set to the user-specified port for AG

--- scripts/step2_ag-setup.sh
+++ scripts/step2_ag-setup.sh
@@ -9,6 +9,7 @@ if ! [[ -e README.md ]]; then
     exit 1
 fi
 
+source allegrograph/build/config/user-env.sh
 
 # Create a Docker volume where AllegroGraph will store its data: 
 docker create --name agraph-data franzinc/agraph-data
@@ -20,15 +21,15 @@ docker build -t ccp/agraph:v6.1.1 allegrograph/build/
 docker run --rm -v $(pwd):/backup billbaumgartner/kabob-base:0.1 tar czvf /backup/kabob.git-backup.tar.gz /kabob.git
 docker create -v /kabob.git --name kabob.git ubuntu:latest
 docker run --rm --volumes-from kabob.git -v $(pwd):/backup ubuntu:latest bash -c "cd /kabob.git && tar xzvf /backup/kabob.git-backup.tar.gz --strip 1"
-rm kabob.git-backup.tar.gz
+rm -f kabob.git-backup.tar.gz
 
 docker run --rm -v $(pwd):/backup billbaumgartner/kabob-base:0.1 tar czvf /backup/m2-backup.tar.gz /root/.m2
 docker create -v /root/.m2 --name m2 ubuntu:latest
 docker run --rm --volumes-from m2 -v $(pwd):/backup ubuntu bash -c "cd /root/.m2 && tar xzvf /backup/m2-backup.tar.gz --strip 2"
-rm m2-backup.tar.gz
+rm -f m2-backup.tar.gz
 
 # Start up AllegroGraph
-docker run -d -p 10000-10035:10000-10035 \
+docker run -d -p 10000-$PLATFORM_ALLEGROGRAPH_PORT:10000-$PLATFORM_ALLEGROGRAPH_PORT \
    --volumes-from agraph-data --volumes-from kabob_data --volumes-from kabob.git --volumes-from m2 \
    --name agraph ccp/agraph:v6.1.1
 
