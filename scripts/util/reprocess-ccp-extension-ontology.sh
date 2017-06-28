cd0f96c296ab42e0e88aa5ca6968e1cd5606faeb
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-28 00:14:38 -0600

    Put quotes around ccp-extension ontology URL

--- scripts/util/reprocess-ccp-extension-ontology.sh
+++ scripts/util/reprocess-ccp-extension-ontology.sh
@@ -7,6 +7,6 @@ DATASOURCE_KEY=$2
 
 DID=""
 echo "Starting kabob-base container to process ccp-extension ontology..."
-DID=${DID}" "`docker run -d --name "ccp_ont_dload_${DATASOURCE_KEY}" --volumes-from kabob_data-${KB_KEY} billbaumgartner/kabob-base:0.3 ./download-single-ontology.sh https://raw.githubusercontent.com/UCDenver-ccp/ccp-extension-ontology/master/src/ontology/ccp-extensions.owl`
+DID=${DID}" "`docker run -d --name "ccp_ont_dload_${DATASOURCE_KEY}" --volumes-from kabob_data-${KB_KEY} billbaumgartner/kabob-base:0.3 ./download-single-ontology.sh "https://raw.githubusercontent.com/UCDenver-ccp/ccp-extension-ontology/master/src/ontology/ccp-extensions.owl"`
 docker wait $DID
 docker rm $DID
