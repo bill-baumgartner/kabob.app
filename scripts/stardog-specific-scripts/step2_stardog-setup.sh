9be6271ade99debc7b9af1c8560298394a2aead4
bill-baumgartner <bill.baumgartner@gmail.com>
2017-10-23 08:41:39 -0600

    Added input argument to step 2 to specify stardog port

--- scripts/stardog-specific-scripts/step2_stardog-setup.sh
+++ scripts/stardog-specific-scripts/step2_stardog-setup.sh
@@ -13,12 +13,16 @@ function print_usage {
     echo "Usage:"
     echo "$(basename $0) [OPTIONS]"
     echo "  [-k <kb-key>]: A unique key that will be used to name docker containers for this build"
+    echo "  [-p <stardog-port>]: The port to use when starting stardog"
 }
 
-while getopts "k:h" OPTION; do
+while getopts "k:p:h" OPTION; do
     case ${OPTION} in
         # A unique key that will be used to name docker containers for this build
         k) KB_KEY=$OPTARG
+           ;;
+        # StarDog port
+        p) STARDOG_PORT=$OPTARG
            ;;
         # HELP!
         h) print_usage; exit 0
@@ -26,7 +30,7 @@ while getopts "k:h" OPTION; do
     esac
 done
 
-if [[ -z ${KB_KEY} ]]; then
+if [[ -z ${KB_KEY} || -z ${STARDOG_PORT} ]]; then
     print_usage
     exit 1
 fi
@@ -43,18 +47,19 @@ docker create -v /stardog-data --name stardog-data-${KB_KEY} ubuntu:latest
 docker create -v /stardog-load-requests --name stardog-load-requests-${KB_KEY} ubuntu:latest
 
 # Build the Docker image (this will import the AllegroGraph Docker image): 
-docker build -t ccp/stardog:v5.0 stardog/
+docker build --build-arg STARDOG_PORT=${STARDOG_PORT} -t ccp/stardog:v5.0 stardog/
 
 # Create a dedicated network so that other containers can talk to the agraph container
 docker network create stardog-net-${KB_KEY}
 
 # Start up Stardog
-docker run -d -p 5820:5820 \
+docker run -d -p ${STARDOG_PORT}:${STARDOG_PORT} \
+       -e STARDOG_PORT=${STARDOG_PORT} \
        --net stardog-net-${KB_KEY} \
        --volumes-from stardog-data-${KB_KEY} --volumes-from kabob_data-${KB_KEY} --volumes-from stardog-load-requests-${KB_KEY} \
        --name stardog-${KB_KEY} ccp/stardog:v5.0
 
 # Log the AllegroGraph port to the ag-load-requests directory
-docker exec stardog-${KB_KEY} /bin/bash -c "echo 5820 > /stardog-load-requests/stardog.port"
+docker exec stardog-${KB_KEY} /bin/bash -c "echo ${STARDOG_PORT} > /stardog-load-requests/stardog.port"
 docker exec stardog-${KB_KEY} /bin/bash -c "echo 'stardog-${KB_KEY}' > /stardog-load-requests/stardog.container.name"
 
--- stardog/scripts/init-stardog.sh
+++ stardog/scripts/init-stardog.sh
@@ -6,6 +6,29 @@
 # stop` is called, allowing stardog to be shutdown gracefully
 #
 
+function print_usage {
+    echo "Usage:"
+    echo "$(basename $0) [OPTIONS]"
+    echo "  [-p <stardog-port>]: The port to use when starting stardog"
+}
+
+while getopts "k:p:h" OPTION; do
+    case ${OPTION} in
+        # StarDog port
+        p) STARDOG_PORT=$OPTARG
+           ;;
+        # HELP!
+        h) print_usage; exit 0
+           ;;
+    esac
+done
+
+if [[ -z ${STARDOG_PORT} ]]; then
+    print_usage
+    exit 1
+fi
+
+
 # catch the termination signal and call a script to stop all monitored processes gracefully
 trap '/stardog-${STARDOG_VERSION}/bin/stardog-admin server stop ; exit' 15
 
@@ -14,7 +37,7 @@ trap '/stardog-${STARDOG_VERSION}/bin/stardog-admin server stop ; exit' 15
 rm -f ${STARDOG_HOME}/system.lock || true && \
     cp /stardog-${STARDOG_VERSION}/stardog-license-key.bin ${STARDOG_HOME} && \
     cp /stardog-${STARDOG_VERSION}/stardog.properties ${STARDOG_HOME} && \
-    /stardog-${STARDOG_VERSION}/bin/stardog-admin server start  && \
+    /stardog-${STARDOG_VERSION}/bin/stardog-admin server start --port ${STARDOG_PORT} && \
     sleep 1 && \
     (tail -f ${STARDOG_HOME}/stardog.log &) && \
     while (pidof java > /dev/null); do sleep 1; done
