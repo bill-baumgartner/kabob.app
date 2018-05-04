2e0b34a36ae0acedcb4533cc4980fa03a1273e7d
bill-baumgartner <bill.baumgartner@gmail.com>
2018-05-04 11:00:11 -0600

    Squash - updated to ontology reprocess script

--- scripts/util/reprocess-all-ontologies.sh
+++ scripts/util/reprocess-all-ontologies.sh
@@ -3,10 +3,9 @@
 # This script provides a way to re-download and process the ccp-extension ontology
 
 KB_KEY=$1
-DATASOURCE_KEY=$2
 
 DID=""
 echo "Starting kabob-base container to process all ontologies..."
-DID=${DID}" "`docker run -d --name "ontology_processing_${DATASOURCE_KEY}" --volumes-from kabob_data-${KB_KEY} billbaumgartner/kabob-base:0.3 ./download-ontologies.sh`
+DID=${DID}" "`docker run -d --name "ontology_processing" --volumes-from kabob_data-${KB_KEY} billbaumgartner/kabob-base:0.3 ./download-ontologies.sh`
 docker wait $DID
 docker rm $DID
