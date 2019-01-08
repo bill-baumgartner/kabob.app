3ae084fc67af067ec5960bb2d9935b2bb87df2bf
bill-baumgartner <bill.baumgartner@gmail.com>
2019-01-08 11:51:56 -0700

    Add memory parameters for bg
    Query max men set to 20G
    Max direct memory size set to 40G

--- blazegraph/scripts/init-blazegraph.sh
+++ blazegraph/scripts/init-blazegraph.sh
@@ -29,7 +29,7 @@
 # start blazegraph by starting jetty
 chown jetty:jetty /blazegraph-data
 cd /var/lib/jetty
-java -Xmx40G -Dcom.bigdata.rdf.sail.webapp.ConfigParams.propertyFile=/RWStore.properties -Dlog4j.configuration=file:///log4j.properties -DSTOP.KEY=KEY -DSTOP.PORT=2222 -jar /usr/local/jetty/start.jar
+java -Xmx40G -XX:MaxDirectMemorySize=40G -Dcom.bigdata.rdf.sail.webapp.ConfigParams.propertyFile=/RWStore.properties -Dlog4j.configuration=file:///log4j.properties -DSTOP.KEY=KEY -DSTOP.PORT=2222 -jar /usr/local/jetty/start.jar
 
 ## create a symbolic link to the deployed blazegraph lib/ directory to /blazegraph-lib
 #CP_DIR=$(grep bigdata.war /var/log/supervisor/blazegraph_process.err.log | cut -f 2 -d "{" | cut -f 2 -d "," | cut -f 3- -d "/")
