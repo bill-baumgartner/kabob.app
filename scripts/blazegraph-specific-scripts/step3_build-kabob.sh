b00a2e1322cac6e3447898a2a132b0c02ebb5a95
bill-baumgartner <bill.baumgartner@gmail.com>
2019-01-16 14:18:54 -0700

    Revised step3 script to pass along command to container shell

--- scripts/blazegraph-specific-scripts/step3_build-kabob.sh
+++ scripts/blazegraph-specific-scripts/step3_build-kabob.sh
@@ -8,18 +8,18 @@ function print_usage {
     echo "Usage:"
     echo "$(basename $0) [OPTIONS]"
     echo "  [-k <kb-key>]: A unique key that will be used to name docker containers for this build"
-    echo "  [-n <kb-name>]: The name of the repository that will be populated as KaBOB, e.g kabob-prod"
+    echo "  [-c <command-to-run>]: The shell command to run, e.g. boot..."
     echo "  [-v <kabob docker image version>]: The version of the kabob docker image to use."
 
 }
 
-while getopts "k:n:v:h" OPTION; do
+while getopts "k:c:v:h" OPTION; do
     case ${OPTION} in
         # A unique key that will be used to name docker containers for this build
         k) KB_KEY=$OPTARG
            ;;
-        # A unique name of the repository that will be populated as KaBOB, e.g kabob-prod
-        n) KB_NAME=$OPTARG
+        # The shell command to run
+        n) SHELL_COMMAND=$OPTARG
            ;;
         # kabob docker image version
         v) VERSION=$OPTARG
@@ -30,7 +30,7 @@ while getopts "k:n:v:h" OPTION; do
     esac
 done
 
-if [[ -z ${KB_KEY} || -z ${KB_NAME} || -z ${VERSION} ]]; then
+if [[ -z ${KB_KEY} || -z ${SHELL_COMMAND} || -z ${VERSION} ]]; then
     print_usage
     exit 1
 fi
@@ -40,5 +40,4 @@ if ! [[ -e README.md ]]; then
     exit 1
 fi
 
-# Build KaBOB using the RDF generated from downloaded data sources:
-docker run --rm --net blazegraph-net-${KB_KEY} --volumes-from kabob_data-${KB_KEY} --volumes-from blazegraph-load-requests-${KB_KEY} billbaumgartner/kabob-base:${VERSION} /kabob.git/scripts/docker/blazegraph-specific/build-from-scratch.sh ${KB_NAME}
+docker run --rm --net blazegraph-net-${KB_KEY} --volumes-from kabob_data-${KB_KEY} --volumes-from blazegraph-load-requests-${KB_KEY} billbaumgartner/kabob-base:${VERSION} sh -c "${SHELL_COMMAND}"
