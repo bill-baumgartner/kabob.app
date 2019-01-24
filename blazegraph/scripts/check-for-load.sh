731f039323a6b01ccfb5575dc1b3ba99303c2fd6
bill-baumgartner <bill.baumgartner@gmail.com>
2019-01-24 09:21:04 -0700

    Squash - debugging load

--- blazegraph/scripts/check-for-load.sh
+++ blazegraph/scripts/check-for-load.sh
@@ -156,7 +156,7 @@ EOF
         # remove the .load file once all parameters have been extracted from it
         echo "Deleting load request: ${path}${file}"
         rm ${path}${file}
-        rm ${LOAD_REQUEST_DIRECTORY}/dataloader.xml
+        #rm ${LOAD_REQUEST_DIRECTORY}/dataloader.xml
 
 	    if [[ ${PIPESTATUS[0]} == 0 ]]
 	    then
